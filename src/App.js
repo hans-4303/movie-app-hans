90c7463555dae2a7acce88e5ad64458d31d8bd64
definitiomea <103019360+definitiomea@users.noreply.github.com>
2022-12-05 00:22:06 +0900

    Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Home from './pages/Home';
+import About from './pages/About';
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <Home></Home>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
